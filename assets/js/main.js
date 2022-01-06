754201a67e392c51b0a26af2f5b7de95b627ebc1
Zaquariah Holland <zaquariah@gmail.com>
2022-01-05 21:30:39 -0600

    Changed the background, again
    - improved performance with this CSS generated background
    - changed alignment of hero section
    - changed colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,35 +2,3 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
-
-// configuration for wave background
-const effect = VANTA.WAVES({
-  el: '#wave-background',
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  color: 0x0,
-  waveHeight: 20,
-  shininess: 50,
-  waveSpeed: 1,
-  zoom: 0.75
-})
-
-// work to check FPS for the background...seems to remove itself on mobile tho...
-let before = Date.now();
-let now;
-let fps = 0;
-
-// requestAnimationFrame(function loop() {
-//   now = Date.now();
-//   fps = Math.round(1000 / (now - before));
-//   before = now;
-//   requestAnimationFrame(loop);
-//   // console.log('Current FPS: ', fps);
-//
-//   if(fps < 20) {
-//     effect.setOptions({
-//       waveSpeed: 0.00
-//     })
-//   }
-// })

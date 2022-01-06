3f94db490d4464af35c027e7736dbb2c7a612302
Zaquariah Holland <zaquariah@gmail.com>
2022-01-05 18:17:25 -0600

    Configurations for background
    - updated background sizing
    - checked for frames before termination
    - changed container viewheight

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,35 @@
+// to open/close the mobile menu
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+// configuration for wave background
+const effect = VANTA.WAVES({
+  el: '#wave-background',
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  color: 0x0,
+  waveHeight: 20,
+  shininess: 50,
+  waveSpeed: 1,
+  zoom: 0.75
+})
+
+let before = Date.now();
+let now;
+let fps = 0;
+
+requestAnimationFrame(function loop() {
+  now = Date.now();
+  fps = Math.round(1000 / (now - before));
+  before = now;
+  requestAnimationFrame(loop);
+  // console.log('Current FPS: ', fps);
+
+  if(fps < 20) {
+    effect.setOptions({
+      waveSpeed: 0.00
+    })
+  }
+})

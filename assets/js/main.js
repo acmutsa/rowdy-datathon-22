d51f6fda890d2b9539bf8c179158bafb226152ff
Zaquariah Holland <zaquariah@gmail.com>
2022-01-17 15:53:22 -0600

    Updated several pages
    - added partners section with pictures as placeholders
    - updated links to the guide to be prepared
    - added content to faq page

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,15 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+// for the faq page to allow the questions to accordion open
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
